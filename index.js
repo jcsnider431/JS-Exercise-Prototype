e98cbc3d5886ec4ba3144ea5bff7a450eabd0469
Chase Snider <jcsnider@outlook.com>
2020-11-11 17:24:58 -0500

    Jonathan-Snider

--- index.js
+++ index.js
@@ -39,10 +39,26 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 
 }
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
 
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
+ const personOne = new Person('Mary', 50);
+ 
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -57,10 +73,24 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 
 }
 
+Car.prototype.fill = function(gallons){
+    return this.tank = this.tank + gallons;
+}
+Car.prototype.drive = function(distance,gallons) {
+  this.odometer += distance;  
+  this.tank = this.tank - gallons;
+    if(this.tank = 0){
+      return `I ran out of fuel at ${this.odometer} miles!`
+    }
+  }
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
@@ -68,11 +98,26 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+    Person.call(this,name ,age);
+    this.favoriteToy = favoriteToy;
 
 }
+Baby.prototype = Object.create(Person.prototype);
 
-/* 
+Baby.prototype.play = function (){
+   return (`Playing with ${this.favoriteToy}`);
+
+}
+const babyOne = new Baby ({
+  name: 'Chase',
+  age: 1,
+  favoriteToy: 'crayon'
+
+})
+babyOne.play();
+  
+  /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
